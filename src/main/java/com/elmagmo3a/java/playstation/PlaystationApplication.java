06c9c9b27bbca0a9544e959d28ec6deed7e39997
Khaled Mostafa <khaledmustafa803@gmail.com>
2019-10-28 14:40:21 +0200

    master

---
+++ src/main/java/com/elmagmo3a/java/playstation/PlaystationApplication.java
@@ -0,0 +1,13 @@
+package com.elmagmo3a.java.playstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaystationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaystationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elmagmo3a/java/playstation/PlaystationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmagmo3a.java.playstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaystationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
